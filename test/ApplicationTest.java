9d9b3ebf445008b36b3a9c36487ef6182d51b0c6
daniel9x <dsgrant@emory.edu>
2016-05-26 16:23:01 -0400

    no message

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,22 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
 import play.twirl.api.Content;
 
-import static play.test.Helpers.*;
 import static org.junit.Assert.*;
 
 
@@ -36,7 +20,7 @@ public class ApplicationTest {
 
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = views.html.welcomePage.render("Your new application is ready.");
         assertEquals("text/html", html.contentType());
         assertTrue(html.body().contains("Your new application is ready."));
     }
